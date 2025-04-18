88c69e14bae7f23ab6eb4ec248c4d98767b8fc3b
28lfss <28lfss@gmail.com>
2025-04-18 18:14:29 -0300

    Update schema

--- pgi/dbschema/fispq_schema.sql
+++ pgi/dbschema/fispq_schema.sql
@@ -13,8 +13,8 @@ CREATE TABLE users (
     hash_password TEXT,
     session_token VARCHAR(256),
     login_timestamp BIGINT,
-    access_level access_level NOT NULL,
-    is_active BOOLEAN DEFAULT FALSE,
+    access_level access_level DEFAULT 'COLLAB'
+    is_active BOOLEAN DEFAULT TRUE,
     device TEXT
 );
 
