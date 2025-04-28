fb9d602b605dae90eecb8fa6ab45f118ae413475
28lfss <28lfss@gmail.com>
2025-04-28 18:28:58 -0300

    Update db schema - Edit dependencies text style;

--- pgi/dbschema/db_schema.sql
+++ pgi/dbschema/db_schema.sql
@@ -1,40 +1,38 @@
-CREATE EXTENSION IF NOT EXISTS "pgcrypto";
-
 CREATE TYPE access_level AS ENUM ('collab', 'admin');
 CREATE TYPE security_sheets_status AS ENUM ('active', 'expired', 'revision');
 CREATE TYPE occurrence_status AS ENUM ('open', 'revision', 'closed');
 
 CREATE TABLE users (
-    user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id SERIAL PRIMARY KEY,
     username VARCHAR(128) NOT NULL,
-    email TEXT UNIQUE NOT NULL,
-    hash_password TEXT,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    hash_password VARCHAR(255),
     session_token VARCHAR(255),
     login_timestamp BIGINT,
     access_level access_level DEFAULT 'collab',
     active BOOLEAN DEFAULT TRUE,
-    device TEXT
+    device VARCHAR(255)
 );
 
 CREATE TABLE security_sheets (
-    security_sheets_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    security_sheets_id SERIAL PRIMARY KEY,
     product_name VARCHAR(255) NOT NULL,
     manufacturer VARCHAR(255) NOT NULL,
     product_type VARCHAR(128),
-    document_attachment VARCHAR(255) NOT NULL,
-    register_data TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    document_path VARCHAR(255) NOT NULL,
+    register_data BIGINT,
     status security_sheets_status NOT NULL,
-    registrant_id UUID,
-    CONSTRAINT fk_user FOREIGN KEY (registrant_id) REFERENCES users(user_id)
+    sheet_registrant_id SERIAL,
+    CONSTRAINT fk_user FOREIGN KEY (sheet_registrant_id) REFERENCES users(user_id)
 );
 
 CREATE TABLE occurrences (
-    occurrence_id BIGINT PRIMARY KEY,
+    occurrence_id SERIAL PRIMARY KEY,
     area VARCHAR(64) NOT NULL,
     description VARCHAR(255) NOT NULL,
-    occurrence_image_url VARCHAR(255) NOT NULL,
-    time_created BIGINT NOT NULL,
-    occurrence_status occurrence_status DEFAULT 'open',
-    registrant_id UUID,
-    CONSTRAINT fk_user FOREIGN KEY (registrant_id) REFERENCES users(user_id)
+    image_path VARCHAR(255) NOT NULL,
+    time_created BIGINT,
+    status occurrence_status DEFAULT 'open',
+    occurrence_registrant_id SERIAL,
+    CONSTRAINT fk_user FOREIGN KEY (occurrence_registrant_id) REFERENCES users(user_id)
 );
