082ebef6f4d9208effd1e47079b73ae0c80d94e1
28lfss <28lfss@gmail.com>
2025-04-29 16:55:06 -0300

    Add validation to Occurrence Request

--- pgi/src/main/java/com/lfssa/pgi/dto/OccurrenceRequest.java
+++ pgi/src/main/java/com/lfssa/pgi/dto/OccurrenceRequest.java
@@ -1,8 +1,17 @@
 package com.lfssa.pgi.dto;
 
+import jakarta.validation.constraints.NotNull;
+
 public class OccurrenceRequest {
+    @NotNull(message = "Occurrence should have a area")
     public String area;
+
+    @NotNull(message = "Occurrence should have a description")
     public String description;
+
+    @NotNull(message = "Occurrence should have a image path")
     public String imagePath;
+
+    @NotNull(message = "Occurrence should have a registrant ID")
     public long occurrenceRegistrantId;
 }
