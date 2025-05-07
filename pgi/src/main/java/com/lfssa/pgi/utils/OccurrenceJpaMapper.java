2ca74722179330fd3162327cbf739c47c2fa1345
28lfss <28lfss@gmail.com>
2025-05-07 09:58:02 -0300

    Update: Api responses
    - custom method for occurrence Response mapper

--- pgi/src/main/java/com/lfssa/pgi/utils/OccurrenceJpaMapper.java
+++ pgi/src/main/java/com/lfssa/pgi/utils/OccurrenceJpaMapper.java
@@ -3,7 +3,10 @@ package com.lfssa.pgi.utils;
 import com.lfssa.pgi.adapters.outbound.entities.JpaOccurrenceEntity;
 import com.lfssa.pgi.domain.occurrence.Occurrence;
 import com.lfssa.pgi.domain.occurrence.OccurrenceResponseDTO;
+import com.lfssa.pgi.domain.user.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
 @Mapper(componentModel = "spring")
 public interface OccurrenceJpaMapper {
@@ -11,5 +14,11 @@ public interface OccurrenceJpaMapper {
 
     JpaOccurrenceEntity occurrenceToJpaOccurrence(Occurrence occurrence);
 
+    @Mapping(source = "user", target = "registrantId", qualifiedByName = "getRegistrantId")
     OccurrenceResponseDTO occurrenceToResponse(Occurrence occurrence);
+
+    @Named("getRegistrantId")
+    public static long getRegistrantId(User user) {
+        return user.getUserId();
+    }
 }
