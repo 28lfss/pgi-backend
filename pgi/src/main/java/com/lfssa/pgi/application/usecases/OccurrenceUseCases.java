61ceaf7ca23203bae25762b601a080a5a7be0e3c
28lfss <28lfss@gmail.com>
2025-05-10 00:21:17 -0300

    feature: Find Occurrences By User Id
    - Changed controllers response type;

--- pgi/src/main/java/com/lfssa/pgi/application/usecases/OccurrenceUseCases.java
+++ pgi/src/main/java/com/lfssa/pgi/application/usecases/OccurrenceUseCases.java
@@ -2,12 +2,13 @@ package com.lfssa.pgi.application.usecases;
 
 import com.lfssa.pgi.domain.occurrence.OccurrenceRequestDTO;
 import com.lfssa.pgi.domain.occurrence.OccurrenceResponseDTO;
-import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
 public interface OccurrenceUseCases {
-    ResponseEntity<String> createOccurrence(OccurrenceRequestDTO request);
+    OccurrenceResponseDTO createOccurrence(OccurrenceRequestDTO request);
 
     List<OccurrenceResponseDTO> findAllOccurrences();
+
+    List<OccurrenceResponseDTO> FindOccurrencesByUserId(long userId);
 }
