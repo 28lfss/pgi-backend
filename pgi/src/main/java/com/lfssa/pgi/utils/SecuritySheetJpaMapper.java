e79feb7f81393f73771c920b6e2dbe4aaa5546fc
28lfss <28lfss@gmail.com>
2025-05-27 13:32:21 -0300

    feat: mapper for request to model

--- pgi/src/main/java/com/lfssa/pgi/utils/SecuritySheetJpaMapper.java
+++ pgi/src/main/java/com/lfssa/pgi/utils/SecuritySheetJpaMapper.java
@@ -2,6 +2,7 @@ package com.lfssa.pgi.utils;
 
 import com.lfssa.pgi.adapters.outbound.entities.JpaSecuritySheetEntity;
 import com.lfssa.pgi.domain.securitysheet.SecuritySheet;
+import com.lfssa.pgi.domain.securitysheet.SecuritySheetRequestDTO;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
@@ -9,4 +10,6 @@ public interface SecuritySheetJpaMapper {
     SecuritySheet jpaToSecuritySheet(JpaSecuritySheetEntity securitySheet);
 
     JpaSecuritySheetEntity securitySheetToJpa(SecuritySheet securitySheet);
+
+    SecuritySheet requestToSecuritySheet(SecuritySheetRequestDTO request);
 }
