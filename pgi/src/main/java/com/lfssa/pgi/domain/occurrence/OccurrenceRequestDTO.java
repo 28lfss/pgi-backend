e69c72fd4958863e9c1d3b2bce4b2f45cc36758d
28lfss <28lfss@gmail.com>
2025-06-07 10:04:09 -0300

    update: Occurrence Request
    - Changed imagePath(String) to imageFile(MultipartFile);
    
    - Add constructor;

--- pgi/src/main/java/com/lfssa/pgi/domain/occurrence/OccurrenceRequestDTO.java
+++ pgi/src/main/java/com/lfssa/pgi/domain/occurrence/OccurrenceRequestDTO.java
@@ -1,17 +1,20 @@
 package com.lfssa.pgi.domain.occurrence;
 
-import jakarta.validation.constraints.NotNull;
+import org.springframework.web.multipart.MultipartFile;
 
 public class OccurrenceRequestDTO {
-    @NotNull(message = "Occurrence should have a area")
     public String area;
 
-    @NotNull(message = "Occurrence should have a description")
     public String description;
 
-    @NotNull(message = "Occurrence should have a image path")
-    public String imagePath;
-
-    @NotNull(message = "Occurrence should have a registrant ID")
     public long occurrenceRegistrantId;
+
+    public MultipartFile imageFile;
+
+    public OccurrenceRequestDTO(String area, String description, long occurrenceRegistrantId, MultipartFile imageFile) {
+        this.area = area;
+        this.description = description;
+        this.occurrenceRegistrantId = occurrenceRegistrantId;
+        this.imageFile = imageFile;
+    }
 }
