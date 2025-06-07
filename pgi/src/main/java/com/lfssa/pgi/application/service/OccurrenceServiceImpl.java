457b0f185915fe4867569d1ce1584bb26d7d3e03
28lfss <28lfss@gmail.com>
2025-06-07 13:33:33 -0300

    feat: creating occurrence now stores the file

--- pgi/src/main/java/com/lfssa/pgi/application/service/OccurrenceServiceImpl.java
+++ pgi/src/main/java/com/lfssa/pgi/application/service/OccurrenceServiceImpl.java
@@ -1,11 +1,12 @@
 package com.lfssa.pgi.application.service;
 
+import com.lfssa.pgi.application.usecases.FileStorageUseCases;
 import com.lfssa.pgi.application.usecases.OccurrenceUseCases;
 import com.lfssa.pgi.domain.occurrence.Occurrence;
 import com.lfssa.pgi.domain.occurrence.OccurrenceRepository;
-import com.lfssa.pgi.domain.user.UserRepository;
 import com.lfssa.pgi.domain.occurrence.OccurrenceRequestDTO;
 import com.lfssa.pgi.domain.occurrence.OccurrenceResponseDTO;
+import com.lfssa.pgi.domain.user.UserRepository;
 import com.lfssa.pgi.infrastructure.config.exceptions.OccurrenceNotFoundException;
 import com.lfssa.pgi.infrastructure.config.exceptions.UserNotFoundException;
 import com.lfssa.pgi.utils.OccurrenceJpaMapper;
@@ -27,19 +28,25 @@ public class OccurrenceServiceImpl implements OccurrenceUseCases {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private FileStorageUseCases fileStorage;
+
     public OccurrenceResponseDTO createOccurrence(OccurrenceRequestDTO request) {
-        if (!userRepository.existsUserById(request.occurrenceRegistrantId)) throw new UserNotFoundException("User not found");
+        if (!userRepository.existsUserById(request.occurrenceRegistrantId))
+            throw new UserNotFoundException("User not found");
+
+        String filePath = fileStorage.store(request.imageFile);
 
         Occurrence newOccurrence = new Occurrence();
         newOccurrence.setArea(request.area);
         newOccurrence.setDescription(request.description);
-        newOccurrence.setImagePath(request.imagePath);
+        newOccurrence.setImagePath(filePath);
         newOccurrence.setTimeCreated(System.currentTimeMillis());
         newOccurrence.setStatus(Occurrence.Status.open);
         newOccurrence.setUser(userRepository.findUserById(request.occurrenceRegistrantId).get());
-        Occurrence occurrence =  occurrenceRepository.createOccurrence(newOccurrence);
 
-        return occurrenceMapper.occurrenceToResponse(occurrence);
+        Occurrence response = occurrenceRepository.createOccurrence(newOccurrence);
+        return occurrenceMapper.occurrenceToResponse(response);
     }
 
     public List<OccurrenceResponseDTO> findAllOccurrences() {
@@ -61,7 +68,8 @@ public class OccurrenceServiceImpl implements OccurrenceUseCases {
     }
 
     public OccurrenceResponseDTO FindOccurrenceById(long occurrenceId) {
-        if (!occurrenceRepository.ExistsById(occurrenceId)) throw new OccurrenceNotFoundException("Occurrence not found");
+        if (!occurrenceRepository.ExistsById(occurrenceId))
+            throw new OccurrenceNotFoundException("Occurrence not found");
 
         return occurrenceMapper.occurrenceToResponse(occurrenceRepository.FindOccurrenceById(occurrenceId));
     }
